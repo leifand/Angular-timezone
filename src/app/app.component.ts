9c39286056083a630ddbfb33f24e296ae123758f
Leif Anderson <leif.v.anderson@gmail.com>
2017-09-17 00:44:19 -0500

    us timezones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zones';
+  time = Date.now();
+  current_zone = 0;
+
+  getStyle(key: number) {
+    if(key == this.current_zone && key !=0) return { 'background-color':'green' };
+    return { 'background-color':'white' };
+  }
+
+  setTimeZone(key: number) { // hardcoded for Dallas
+    let offset = 0;
+    if(key == 1) offset = 3600000;
+    else if(key==2) offset = 0;
+    else if(key==3) offset = -3600000;
+    else if(key==4) offset = -7200000;
+    this.time = Date.now() + offset;
+    this.current_zone = key;
+  }
 }
